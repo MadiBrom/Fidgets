abccb0ceb5670e799e0561100f54a01be762432d
MadiBrom <madibromfield215@gmail.com>
2025-11-12 11:48:00 -0600

    path cleanup and test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 ﻿import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Slime from "./components/slime/Slime";
-import Navbar from "./navbar/Navbar";
+import Navbar from "./components/navbar/Navbar"
+import Home from "./components/home/Home";
 import Buttons from "./components/buttons/Buttons";
+import Confetti from "./components/confetti/Confetti";
+import Bubbles from "./components/pops/Bubbles";
 import Birds from "./components/Birds";
 import Lines from "./components/Lines";
 import Ocean from "./components/Ocean";
 import Rainbow from "./components/Rainbow";
 import Drips from "./components/Drips";
-import Spiral from "./components/Spiral";
-import Home from "./components/start/Home";
-import Confetti from "./components/confetti/Confetti";
-import Bubbles from "./components/Pops/bubbles";
+// import Slime from "./components/slime/Slime";
+// import Spiral from "./components/Spiral";
+
 
 function App() {
   const playRef = React.useRef(null);
@@ -20,12 +21,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/slime" element={
-<main ref={playRef} className="slime-play">
-  {/* HUD or other UI here */}
-  <Slime fieldRef={playRef} preset="toy" blobCount={18} quality={1.0} />
-</main>
-        } />       
+        {/* <Route path="/slime" element={
+        <main ref={playRef} className="slime-play">
+          <Slime fieldRef={playRef} preset="toy" blobCount={18} quality={1.0} />
+        </main>
+        } />        */}
         <Route path="/buttons" element={<Buttons />} />
         <Route path="/birds" element={<Birds />} />
         <Route path="/pops" element={<Bubbles />} />
@@ -33,7 +33,7 @@ function App() {
         <Route path="/ocean" element={<Ocean />} />
         <Route path="/rainbow" element={<Rainbow />} />
         <Route path="/drips" element={<Drips />} />
-        <Route path="/spiral" element={<Spiral />} />
+        {/* <Route path="/spiral" element={<Spiral />} /> */}
         <Route path="/confetti" element={<Confetti />} />
       </Routes>
     </div>
